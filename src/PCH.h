98e13a547c2a72e3d9ab168ae16c0df9f52b2a85
vcpkg-action <gottyduke@hotmail.com>
2024-02-01 11:47:09 +0800

    Init

---
+++ src/PCH.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+// clib
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
+#include <REL/Relocation.h>
+
+// winnt
+#include <ShlObj_core.h>
+
+using namespace std::literals;
+using namespace REL::literals;
+
+#define DLLEXPORT extern "C" [[maybe_unused]] __declspec(dllexport)
+
+// Plugin
+#include "Plugin.h"
+
+// DKUtil
+#include "DKUtil/Logger.hpp"
